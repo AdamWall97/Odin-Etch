8181a63f975636ebd2da096beaeddf3058d34b02
Adam W <adamwall97@gmail.com>
2022-01-13 12:34:38 -0600

    Adding etch a sketch

--- EtchScript.js
+++ EtchScript.js
@@ -1,12 +1,70 @@
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+
 const container = document.querySelector('#container');
 
 
-let str = "Box "; //Creates 16 Boxes
+
+
+//Creates 16 Boxes
 for(let i = 0;i<16;i++){
 
         const square = document.createElement('div');
         square.classList.add("Box");
-        square.textContent = "Box: " + (i+1);
+        square.addEventListener('mouseenter',colorchange);        
         container.appendChild(square);
+}
+
+
+function colorchange(){
+  
+  var randomcolor = Math.floor(Math.random()*16777215).toString(16); 
+    event.target.style.backgroundColor = "#"+randomcolor;
+
+    }
+
+
+const button = document.querySelector('#button');
+button.classList.add('clear');
+button.addEventListener('click',cleargrid);
+container.appendChild(button);
+
+
+function cleargrid(){
+
+//clear grid and start new
+    const test = container.children;
+
+
+    for(let i = 0; i < test.length; i++){
+        
+        test[i].style.backgroundColor = 'white';
+    }
+
+let newgrid = window.prompt("Enter grid size:");
+if (newgrid > 100){
+    newgrid = 100;
+}
+container.clearBox
+console.log(newgrid*newgrid);
+//Create the grid asked
+for(let i = 0;i<(newgrid*newgrid);i++){
+
+    const square = document.createElement('div');
+    square.classList.add("Box");
+    square.addEventListener('mouseenter',colorchange);        
+    container.appendChild(square);
+    
+}
+container.style = "grid-template-columns: repeat("+newgrid+","+(100/newgrid)+"%);grid-template-rows: repeat("+newgrid+",1fr);";
+ 
+const button = document.querySelector('#button');
+button.classList.add('clear');
+button.addEventListener('click',cleargrid);
+container.appendChild(button);
+
 }
 
